ab72c26eb2849b0197b9996b158bfdb868a87987
Gabriel M <402666+arlequin@users.noreply.github.com>
2023-12-13 03:01:59 -0300

    Changes in the message text

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World Release 2!"
+    return "Hello World. Starting engines for the launch!"
 # run the app.
 if __name__ == "__main__":
     app.run()
